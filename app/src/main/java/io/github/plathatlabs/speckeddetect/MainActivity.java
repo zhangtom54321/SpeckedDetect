feda8c23bfdadbc22983af3bf5f57b771865e8de
Tom Zhang <zhangtom54321@gmail.com>
2018-06-03 14:07:42 -0700

    Fixed Camera error

--- app/src/main/java/io/github/plathatlabs/speckeddetect/MainActivity.java
+++ app/src/main/java/io/github/plathatlabs/speckeddetect/MainActivity.java
@@ -3,6 +3,7 @@ package io.github.plathatlabs.speckeddetect;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void goToCamera(){
+    public void goToCamera(View v){
         Intent intent = new Intent(MainActivity.this, CameraActivity.class);
         MainActivity.this.startActivity(intent);
     }
